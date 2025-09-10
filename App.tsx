394b8dd4ebdec9482f326522cf2851ddd51132e7
ooliveiratg <menezesth16@gmail.com>
2025-09-10 18:02:56 -0300

    feat:merge

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, Text  } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className='text-cyan-600'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
