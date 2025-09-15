347a0f01f4baa51bc21ecb7739e5a6970f8fdc82
ooliveiratg <menezesth16@gmail.com>
2025-09-15 12:36:57 -0300

    feat(Sing-in): adding buttons to the login screen

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -7,14 +7,27 @@ interface InputTypes {
   type?: "email" | "password";
 }
 
+
+
 export function Input({ placeholder, className, type }: InputTypes) {
+  
   return (
     <View className={`min-w-full flex-row items-center  ${type === "email" ? "rounded-tr-[10px] rounded-tl-[10px]" : "rounded-br-[10px] rounded-bl-[10px]"} ${className}`}>
-      <MaterialIcons name="mail-outline" className="pl-[14px]" color={"#0052B4"}  size={16}  />
+
+
+      {type === "email"? (
+        <MaterialIcons name="mail-outline" className="pl-[14px]" color={"#0052B4"}  size={16}  />
+      ):(
+        <MaterialIcons name="lock-outline" className="pl-[14px]" color={"#0052B4"}  size={16}  />
+      )
+
+    }
+     
       <TextInput
         placeholder={placeholder}
+        secureTextEntry={type === "password"}
         className={'w-full h-[46px]  font-inter pl-[12px]'}
       />
-    </View>
+    </View>  
   );
 }
--- app/screens/SingIn.tsx
+++ app/screens/SingIn.tsx
@@ -4,9 +4,7 @@ import { backgroundLogin } from "../../assets/images";
 import Logo from "../../assets/svg/Logo-white.svg";
 import { Input } from "app/components/input";
 
-
 export default function SingIn() {
-
   return (
     <SafeAreaView className="flex-1">
       <ImageBackground
@@ -15,11 +13,23 @@ export default function SingIn() {
         className="flex-1 w-full h-full"
       >
         <View className="flex-col justify-center items-center gap-[96px] flex-1 ">
-            <Logo />
-            <View className="flex-col mr-[24px] ml-[24px]">
-              <Input placeholder={"Digite seu email"} type="email" className="bg-white  "/>
+          <Logo />
+          <View className="flex-col mr-[6.4%] ml-[6.4%]">
+            <Input
+              placeholder={"Digite seu email"}
+              type="email"
+              className="bg-white"
+            />
+            <Input
+              placeholder={"Digite sua senha"}
+              type="password"
+              className="bg-white"
+            />
+            <View className="flex-row justify-between mt-[24px]">
+              <Text className="text-white"> Esqueceu a senha?</Text>
+               <Text className="text-white"> Cadastre-se </Text>
             </View>
-          
+          </View>
         </View>
       </ImageBackground>
     </SafeAreaView>
