102fd3df3983e6f5e54ea7ca1979d70ae61996e6
ooliveiratg <menezesth16@gmail.com>
2025-09-15 21:08:50 -0300

    feat(Sing-in): customizing the button for the login screen

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,3 +1,25 @@
-export function Button() {
-    
+import { ReactNode } from "node_modules/@types/react";
+import { Button, TouchableOpacity, Text } from "react-native";
+interface ButtonProps {
+    classNameButton?: string;
+    classNameText?: string;
+    title: ReactNode;
+    onPress?: () => void;
+    activeOpacity?: number;
+}
+
+export function ButtonStyle({ title, classNameText,
+    onPress, classNameButton,activeOpacity }: ButtonProps) {
+    return (
+        <TouchableOpacity 
+        className={classNameButton}
+        onPress={onPress}
+        activeOpacity={activeOpacity}
+        >
+            <Text className={classNameText}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+
+    )
 }
--- app/screens/SingIn.tsx
+++ app/screens/SingIn.tsx
@@ -3,18 +3,27 @@ import { ImageBackground, SafeAreaView, Text, View } from "react-native";
 import { backgroundLogin } from "../../assets/images";
 import Logo from "../../assets/svg/Logo-white.svg";
 import { Input } from "app/components/input";
+import { useRouter } from 'expo-router'
+import { ButtonStyle } from "app/components/button";
+import { LinearGradient } from "expo-linear-gradient"
+
 
 export default function SingIn() {
+  const router = useRouter();
+
   return (
-    <SafeAreaView className="flex-1">
+    <SafeAreaView
+      className="flex-1">
       <ImageBackground
         source={backgroundLogin}
         resizeMode="cover"
         className="flex-1 w-full h-full"
       >
-        <View className="flex-col justify-center items-center gap-[96px] flex-1 ">
+        <View
+          className="flex-col justify-center items-center gap-[96px] flex-1 ">
           <Logo />
-          <View className="flex-col mr-[6.4%] ml-[6.4%]">
+          <View
+            className="flex-col mr-[9.4%] ml-[9.4%]">
             <Input
               placeholder={"Digite seu email"}
               type="email"
@@ -25,10 +34,32 @@ export default function SingIn() {
               type="password"
               className="bg-white"
             />
-            <View className="flex-row justify-between mt-[24px]">
-              <Text className="text-white"> Esqueceu a senha?</Text>
-               <Text className="text-white"> Cadastre-se </Text>
+            <View
+              className="flex-row justify-between mt-[24px] mb-[24px]">
+              <Text
+                className="text-white  border-b border-b-white text-[12px]"> Esqueceu a senha?</Text>
+
+              <ButtonStyle
+                classNameButton=""
+                activeOpacity={0.7}
+                classNameText="text-white border-b border-b-white text-[12px]"
+                onPress={() => router.navigate('/')} title={'Cadastre-se'}>
+              </ButtonStyle>
             </View>
+
+            <LinearGradient 
+            colors={['rgba(255, 255, 255, 1)','rgba(255, 255, 255, 0.12)']}
+            style={{borderRadius:10}}
+            className=""
+            >
+              <ButtonStyle 
+              title={"Entrar"} 
+              classNameText="text-white font-inter text-[14px]"
+              classNameButton="w-full h-[48px] items-center justify-center rounded-[6px] bg-blue rounded-[10px] shadow "
+              >
+
+              </ButtonStyle >
+            </LinearGradient>
           </View>
         </View>
       </ImageBackground>
