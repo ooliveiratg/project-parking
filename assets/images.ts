fa9cfbf6ebfea77ab331b97d543355ba1dce1527
ooliveiratg <menezesth16@gmail.com>
2025-10-28 05:45:22 -0300

    feat: change in the path to the image

--- assets/images.ts
+++ assets/images.ts
@@ -1,4 +1,3 @@
 export { default as backgroundLogin } from './png/background-login.png'
-export { default as Car } from './png/Car.png'
 export { default as BackgroundVehicle } from './png/background-vehicle.png'
 
